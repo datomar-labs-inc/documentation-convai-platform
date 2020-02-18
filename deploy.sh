defce2d4643acbcbc45d528c4fb8f236454711dd
Caleb Hiebert <calebkhiebert@gmail.com>
2020-02-18 13:00:45 -0600

    6

--- deploy.sh
+++ deploy.sh
@@ -2,38 +2,20 @@ npm ci
 yarn build
 echo "docs.convai.dev" > ./build/CNAME
 
-set -e
-
 git config user.email "dev@datomar.com"
 git config user.name "Dr. Docs Deployus"
 
-remote=$(git config remote.origin.url)
-described_rev=$(git rev-parse HEAD | git name-rev --stdin)
+export TARGET_BRANCH=gh-pages
+git clone https://$_GH_TOKEN@github.com/datomar-labs-inc/documentation-convai-platform out
+cd out
+git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
+git rm -rf .
+cd ..
+cp -a build/. out/.
+cd out
+echo docs.convai.dev > CNAME
+git add -A
+git commit -m "Automated deployment to GitHub Pages"
+git push https://$_GH_TOKEN@github.com/datomar-labs-inc/documentation-convai-platform $TARGET_BRANCH -q
 
-pages_dir="build"
-
-if [ ! -d "$pages_dir" ]
-then
-    echo "Usage: $0 DIRECTORY"
-    exit 1
-fi
-
-cd "$pages_dir"
-
-cdup=$(git rev-parse --show-cdup)
-if [ "$cdup" != '' ]
-then
-    git init
-    git remote add --fetch origin "$remote"
-fi
-
-if git rev-parse --verify origin/gh-pages > /dev/null 2>&1
-then
-    git checkout gh-pages
-else
-    git checkout --orphan gh-pages
-fi
-
-git add .
-git commit -m "pages built at $described_rev" -e
-git push origin gh-pages
+set -e
