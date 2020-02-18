59452cf3c5f30ec1aede095bb525f97c29e2a4f1
Caleb Hiebert <calebkhiebert@gmail.com>
2020-02-18 13:07:35 -0600

    7

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,8 @@ npm ci
 yarn build
 echo "docs.convai.dev" > ./build/CNAME
 
-git config user.email "dev@datomar.com"
-git config user.name "Dr. Docs Deployus"
-
 export TARGET_BRANCH=gh-pages
-git clone https://$_GH_TOKEN@github.com/datomar-labs-inc/documentation-convai-platform out
+git clone https://$GH_TOKEN@github.com/datomar-labs-inc/documentation-convai-platform out
 cd out
 git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
 git rm -rf .
@@ -14,8 +11,10 @@ cd ..
 cp -a build/. out/.
 cd out
 echo docs.convai.dev > CNAME
+git config user.email "dev@datomar.com"
+git config user.name "Dr. Docs Deployus"
 git add -A
 git commit -m "Automated deployment to GitHub Pages"
-git push https://$_GH_TOKEN@github.com/datomar-labs-inc/documentation-convai-platform $TARGET_BRANCH -q
+git push https://$GH_TOKEN@github.com/datomar-labs-inc/documentation-convai-platform $TARGET_BRANCH -q
 
 set -e
